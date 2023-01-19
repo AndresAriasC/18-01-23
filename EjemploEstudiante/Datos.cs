3bddacde911af0d18bb2c1bb4bba88cd3001bf1b
AndresAriasC <110309134+AndresAriasC@users.noreply.github.com>
2023-01-18 18:26:28 -1100

    Add files via upload

---
+++ EjemploEstudiante/Datos.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjemploEstudiante
+{
+    internal class Datos
+    {
+        private String nombre;
+        private String matricula;
+        private int nota;
+        private int cEstu;
+
+        public Datos() { }
+
+        public void setNombre(string Nombre) {this.nombre = Nombre; }
+        public void setMatricula(string Matricula) { this.matricula = Matricula; }
+
+        public int Nota{ get;  set; }
+        public int CEstu { get; set; }
+
+        public int Validar(string Mensaje)
+        {
+            int Num = 0, v;
+
+            do
+            {
+                v = 1;
+
+                try
+                {
+                    Console.Write(Mensaje);
+                    Num = Convert.ToInt32(Console.ReadLine());
+                }
+                catch (Exception error)
+                {
+                    v = 0;
+
+                    Console.WriteLine("Digite un dato valido");
+                }
+
+
+            } while (v == 0);
+
+            return Num;
+        }
+    }
+}
---
+++ EjemploEstudiante/Program.cs
@@ -0,0 +1,49 @@
+﻿
+
+namespace EjemploEstudiante;
+using System;
+class Program { 
+
+    static void Main(string[] args)
+    {
+        int suma, a;
+
+        Datos d = new Datos();
+        d.CEstu = d.Validar("Ingrese la cantidad de estudiantes ");
+
+        for (int i = 0; i < d.CEstu; i++)
+        {
+            suma = 0;
+            Console.WriteLine("Estudiante #" + (i+1));
+            Console.Write("Digite su nombre: ");
+            d.setNombre(Console.ReadLine());
+            Console.Write("Digite el numero de matricula: ");
+            d.setMatricula(Console.ReadLine());
+            //se recorre con un for las notas
+
+            for (int j = 0; i < 4; j++)
+            {
+                do
+                {
+                    d.Nota = d.Validar("Ingresa la nota #" + (j + 1) + " en el rango de 0 a 25: ");
+                    suma += d.Nota;
+                    a = suma;
+                    break;
+
+                } while (d.Nota < 1 || d.Nota > 25);
+                Console.WriteLine("Sumatoria con la nota anterior " + a);
+
+                if (suma < 60)
+                {
+                    Console.WriteLine("REPROBADO");
+                }
+                else Console.WriteLine("APROBADO");
+            }
+
+
+
+        }
+
+
+    }
+}
